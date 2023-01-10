5aa8073c998ed4fd1982d92119be1198de47b99a
mahmoodwadi <122314514+mahmoodwadi@users.noreply.github.com>
2023-01-10 13:03:44 +0200

    Add files via upload

---
+++ hasepa.js
@@ -0,0 +1,25 @@
+
+
+const num1=document.querySelectorAll(".qsma")
+document.addEventListener("click",function qsma(num1,num2){
+    return num1/num2;
+})
+
+
+const num2=document.querySelectorAll()
+
+function darb(num1,num2){
+    return num1*num2;
+}
+
+function sum(num1,num2){
+    return num1+num2;
+}
+
+
+
+function calculater (num1,num2,mwde){
+    return mwde (num1,num2);
+}
+console.log(tarh(16,4));
+console.log(calculater(44,6,sum))
